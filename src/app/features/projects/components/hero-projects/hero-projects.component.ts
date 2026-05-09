c7ad6d0f4047fea6e8909132ffacf8e537cd0a29
ZarKoCool73 <dgonzales.dg7@gmail.com>
2026-05-08 22:40:53 -0500

    feat(main): update views

--- src/app/features/projects/components/hero-projects/hero-projects.component.ts
+++ src/app/features/projects/components/hero-projects/hero-projects.component.ts
@@ -1,9 +1,16 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  ViewChild
+} from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 
 import { Project } from '../../../../core/models/project.model';
 
-import { HeroProjectCardComponent } from '../../shared/hero-project-card/hero-project-card.component';
+import { HeroProjectCardComponent }
+  from '../../shared/hero-project-card/hero-project-card.component';
 
 @Component({
   selector: 'app-hero-projects',
@@ -19,5 +26,26 @@ export class HeroProjectsComponent {
 
   @Input({ required: true })
   projects: Project[] = [];
+
+  @ViewChild('slider', { static: true })
+  slider!: ElementRef<HTMLDivElement>;
+
+  scrollLeft(): void {
+
+    this.slider.nativeElement.scrollBy({
+      left: -500,
+      behavior: 'smooth'
+    });
+
+  }
+
+  scrollRight(): void {
+
+    this.slider.nativeElement.scrollBy({
+      left: 500,
+      behavior: 'smooth'
+    });
+
+  }
 
 }
