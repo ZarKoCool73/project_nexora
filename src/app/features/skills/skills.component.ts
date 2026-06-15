4ce4e66c576ceb669c6d5dcca8bdf6fc4f3e07c8
ZarKoCool73 <dgonzales.dg7@gmail.com>
2026-06-15 16:28:21 -0500

    feat(main): update cv

--- src/app/features/skills/skills.component.ts
+++ src/app/features/skills/skills.component.ts
@@ -22,7 +22,7 @@ export class SkillsComponent {
   selectedCategory = 'All';
   hoveredTech: FlatTech | null = null;
   currentPage = 1;
-  itemsPerPage = 4;
+  itemsPerPage = 20;
 
 
   hoverTech(tech: FlatTech) {
